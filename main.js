4ec67876e688e1d2566098cb81eb8c88ee6be575
ruansx <112948189+ruanssxx@users.noreply.github.com>
2024-04-08 08:42:36 -0300

    545151

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".aba-conteudo");
 
 for(let i=0;i <botoes.length;i++){
  botoes[i].onclick = function()}
@@ -10,6 +11,7 @@ botoes[j].classList.remove("ativo");
   }
 
  botoes[i].classList.add("ativo");
+ textos[i].classList.add("ativo")
 
 {
 
